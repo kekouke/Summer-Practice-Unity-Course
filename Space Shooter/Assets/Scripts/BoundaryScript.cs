9cb872c14f45b3a4b96535ab79fb003d7fcb77eb
Mikhail Kozitskii <kekouke@yandex.ru>
2020-07-21 14:11:46 +1000

    Complete Challenge - Destroy Laser

--- Space Shooter/Assets/Scripts/BoundaryScript.cs
+++ Space Shooter/Assets/Scripts/BoundaryScript.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class BoundaryScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerExit(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(other.gameObject);
     }
 }
