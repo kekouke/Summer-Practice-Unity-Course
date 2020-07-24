0619889f9f62551b114fbc9ed2be052e14a660f1
Mikhail Kozitskii <kekouke@yandex.ru>
2020-07-24 14:04:02 +1000

    Add asteroid destruction

--- Space Shooter/Assets/Scripts/Asteroid.cs
+++ Space Shooter/Assets/Scripts/Asteroid.cs
@@ -6,9 +6,21 @@ public class Asteroid : MonoBehaviour
 {
     [SerializeField]
     private float _speed;
+    [SerializeField]
+    private GameObject _explosion;
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(new Vector3(0, 0, _speed));
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Laser")
+        {
+            Instantiate(_explosion, transform.position, Quaternion.identity);
+            Destroy(gameObject, 0.25f);
+            Destroy(other.gameObject);
+        }
     }
 }
