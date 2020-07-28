fe6debebb1e48a3e2de2e0a643ec22703b0cabfe
Mikhail Kozitskii <kekouke@yandex.ru>
2020-07-28 16:54:29 +1000

    Add moving platform

---
+++ 2.5D Platformer/Assets/Scripts/MovingPlatrofm.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatrofm : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _pointA;
+    [SerializeField]
+    private Transform _pointB;
+
+    private bool direction;
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _pointB.position, 5 * Time.deltaTime);
+
+        if (transform.position == _pointB.position || transform.position == _pointA.position)
+        {
+            (_pointA, _pointB) = (_pointB, _pointA);
+        }
+    }
+}
